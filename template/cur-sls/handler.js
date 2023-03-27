dfcc4a355202b4cc712b7eddd39e17d4a9e16f81
jinho96-kim <jinho96.kim@navercorp.com>
2023-03-27 10:37:06 +0900

    template change test

--- template/cur-sls/handler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function hello(params) {
-    let name = params.name || "World";
-    let place = params.place || "Naver";
-    return {payload:  "Hello, " + name + " in " + place + "!"};
-}
-
-exports.hello = hello;
